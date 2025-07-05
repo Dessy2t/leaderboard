aed09722374b81a9dfb46225647958072b1c5f47
principlebrothers <principlesbrothers@yahoo.com>
2025-07-05 11:55:51 +0000

    Enhance form submission handling and display scores functionality

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
-let yourName = document.getElementById("name")
-let yourScore = document.getElementById("score")
-let submit= document.getElementsByClassName("button")
 let form = document.getElementById("form")
+let inputUser = document.querySelector("input[name='user']")
+let inputScore = document.querySelector("input[name='score']");
+let noticeTag = document.querySelector('#notice')
+let refreshBtn = document.querySelector('#refresh_btn');
+let output = document.querySelector('ul')
+let SCORES_URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/`;
+
+let displayScores = (scores) => {
+  let { result } = scores;
+  result?.slice(4430).map((result) => {
+    let {score, user} = result
+    let li = `<li>${user} : ${String(score).slice(0, 3)} </li>`;
+    output.insertAdjacentHTML('beforeend',li )
+  });
+}
+
+async function retrieveScore(url) {
+  let apiResponse = await fetch(url);
+  let data = await apiResponse.json()
+  return data
+}
+
+let results =  await retrieveScore(SCORES_URL);
+displayScores(results);
+
+refreshBtn.addEventListener('click', async() => {
+  let results =  await retrieveScore(SCORES_URL);
+  displayScores(results);
+});
+
 
 form.addEventListener("submit", function(event){
-    event.preventDefault()
-    console.log("Form submitted")
+  event.preventDefault()
+  let data = {
+    user: inputUser.value,
+    score: inputScore.value,
+  }
+
+  fetch(SCORES_URL, {
+    method: this.method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        noticeTag.textContent = response;
+      }
+      return response.json();
+    })
+    .then((outcome) => {
+      let { result } = outcome;
+      noticeTag.textContent = result;
+    });
 })
